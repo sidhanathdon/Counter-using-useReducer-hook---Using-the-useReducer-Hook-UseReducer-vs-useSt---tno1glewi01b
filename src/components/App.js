25da56804d6f83e3f4de4cdd38751a41135fbd48
sidhanathdon <137877586+sidhanathdon@users.noreply.github.com>
2023-12-13 20:50:23 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,18 @@
-import React from 'react'
+import React, { useReducer } from 'react'
 import '../styles/App.css';
 const App = () => {
+ const initialState={count:0}
+ const [taskState, dispatch]=useReducer(reducer, initialState)
 
+ function reducer(state, action){
+  switch(action.type)
+   {
+    case 'add': return {count: state.count+1}
+    case 'delete': return {count: state.count<1?0:state.count-1}
+    case 'deleteAll': return {count: 0}
+    default: return {count: 0}
+   }
+ }
   return (
     <div id="main">
       <h2>Task Counter</h2>
